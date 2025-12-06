5174eb97245760558de31b05b61c3265a8223ff7
Daniel Springer <dnl.springer@googlemail.com>
2025-12-06 20:35:28 +0100

    mariadb set up - working on its own

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 up:
-	docker compose -f ./srcs/compose.yaml up
+	docker compose -f ./srcs/compose.yaml up --build
 
 down:
 	docker compose -f ./srcs/compose.yaml down
@@ -12,7 +12,6 @@ set-up:
 	-mkdir $(HOME)/data/database
 	touch ./secrets/db_password.txt ./secrets/db_root_password.txt ./secrets/wp_password.txt
 	@echo -e ${RED}These secrets still need to be set.${NC}
-	@echo test
 
 deconst_vol:
 	rm -r $(HOME)/data/database/*
---
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+DATA_PATH="/var/lib/mysql"
+MARIADB_ROOT_PASSWORD=$(cat "${MARIADB_ROOT_PASSWORD_FILE}")
+MARIADB_PASSWORD=$(cat "${MARIADB_PASSWORD_FILE}")
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d "$DATA_PATH/mysql" ]; then
+  echo "initializing MariaDB for the first time"
+  mysql_install_db --user=mysql --datadir="$DATA_PATH"
+  mysqld --user=mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+CREATE DATABASE $MARIADB_DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+exec "$@"
