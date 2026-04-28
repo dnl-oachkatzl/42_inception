899bf68c2f48efca47d7cc0013a406c0ced8ec22
Daniel Springer <dnl.springer@googlemail.com>
2026-04-28 17:07:06 +0200

    nginx and wordpress up and running - w/o database, though.

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -7,5 +7,25 @@ WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 WP_DIR=/var/www/wordpress
 
+# download wordpress if not present
+if [ ! -f "${WP_DIR}/wp-login.php" ]; then
+  echo "downloading wordpress..."
+  wp core download \
+    --path="${WP_DIR}" \
+    --allow-root
+  echo "finished downloading wordpress"
+fi
 
-exec php-fpm7.4 -F
+# configure wordpress if not alreadz done
+# if [ ! -f "${WP_DIR}/wp-config.php" ]; then
+#   echo "configuring wordpress..."
+#   wp config create \
+#     --path="${WP_DIR}" \
+#     --allow-root
+#
+#   echo "done configuring wordpress"
+# fi
+
+chown -R www-data:www-data ${WP_DIR}
+
+exec php-fpm8.2 -F
