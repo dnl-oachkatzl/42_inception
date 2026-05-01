2671ad093f07c04f64d855b9b47c9f2497bd47f9
Daniel Springer <dnl.springer@googlemail.com>
2026-05-01 11:10:41 +0200

    finally, mariadb initializes correctly and only when needed - with a nice script

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -6,49 +6,96 @@ DATA_PATH="/var/lib/mysql"
 MARIADB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 MARIADB_PASSWORD=$(cat /run/secrets/db_password)
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
-chown -R mysql:mysql "$DATA_PATH"
+log() {
+	echo "[entrypoint] $*"
+}
+ 
+database_exists() {
+	[ -d "$DATA_PATH/mysql" ]
+}
 
-if [ ! -f "$DATA_PATH/.initialized" ]; then
-    echo "Initializing MariaDB for the first time..."
+initialize_database() {
+	# touch "$DATA_PATH/mysql/.exists"
+    log "initializing MariaDB for the first time..."
 
     mariadb-install-db \
         --user=mysql \
         --datadir="$DATA_PATH"
 
-    echo "Starting temporary MariaDB server..."
 
-    mysqld --user=mysql --datadir="$DATA_PATH" --skip-networking &
+    log "starting temporary MariaDB server..."
+    
+    mariadbd \
+        --user=mysql \
+        --datadir="$DATA_PATH" \
+        --skip-networking \
+        --socket=/tmp/mysqld.sock &
     pid="$!"
 
-    until mysqladmin ping --silent; do
+    log "waiting for MariaDB to bcome ready..."
+    
+    until mariadb-admin \
+        --socket=/tmp/mysqld.sock \
+        ping \
+        --silent; do
         sleep 1
     done
+    
+    log "initializing..."
 
-    mysql -u root <<EOF
-ALTER USER 'root'@'localhost'
-IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
 
-CREATE DATABASE ${MARIADB_DATABASE}
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-CREATE USER '${MARIADB_USER}'@'%'
-IDENTIFIED BY '${MARIADB_PASSWORD}';
-
-GRANT ALL PRIVILEGES
-ON ${MARIADB_DATABASE}.*
-TO '${MARIADB_USER}'@'%';
-
-FLUSH PRIVILEGES;
+    mariadb --socket=/tmp/mysqld.sock <<EOF
+    	-- set root password
+    	ALTER USER 'root'@'localhost'
+    		IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
+    	
+	-- create wordpress database
+	CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE}
+		CHARACTER SET utf8mb4
+		COLLATE utf8mb4_general_ci;
+	
+	-- create wordpress user
+	CREATE USER '${MARIADB_USER}'@'%'
+		IDENTIFIED BY '${MARIADB_PASSWORD}';
+	
+	GRANT ALL PRIVILEGES
+		ON ${MARIADB_DATABASE}.*
+		TO '${MARIADB_USER}'@'%';
+	
+	FLUSH PRIVILEGES;
 EOF
+    
+    log "shutting down temporary server..."
+    
+    mariadb-admin \
+        --socket=/tmp/mysqld.sock \
+        --user=root \
+        --password="${MARIADB_ROOT_PASSWORD}" \
+        shutdown
+	wait "$pid"
 
-    mysqladmin -u root shutdown
+    log "MariaDB initialization complete."
+}
 
-    touch "$DATA_PATH/.initialized"
+main() {
+	log "container startup"
+	
+   	mkdir -p /run/mysqld
+    	chown mysql:mysql /run/mysqld
+    	
+	if database_exists; then
+		log "existing database found. skipping initialization."
+	else
+		chown -R mysql:mysql "$DATA_PATH"
+		initialize_database
+	fi
+	
+	log "starting mariadb server..."
+	
+	exec mariadbd \
+		--user=mysql \
+		--datadir="$DATA_PATH" \
+		--console
+}
 
-    echo "MariaDB initialization complete."
-fi
-
-exec "$@"
+main
